9766f03b5e219906de16123fa5bb9994b91395c9
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-04 23:42:12 +0900

    Refactoring getMinNumberIndex method

--- src/Sorts/SelectionSort.java
+++ src/Sorts/SelectionSort.java
@@ -4,18 +4,10 @@ public class SelectionSort {
     public static void main(String[] args) {
 
         int[] numbers = {12, 4, 5, 7, 8, 6, 10, 11, 18, 20};
-        int min;
         int index = 0;
-        int temp;
 
         for(int i =0; i< numbers.length; i++) {
-            min= Integer.MAX_VALUE;
-            for(int j=i; j<numbers.length; j++) {
-               if(numbers[j] < min) {
-                   min = numbers[j];
-                   index = j;
-               }
-            }
+            index = getMinNumberIndex(numbers, index, i);
             swap(numbers, index, i);
         }
 
@@ -23,6 +15,18 @@ public class SelectionSort {
             System.out.print(num + " ");
         }
 
+    }
+
+    private static int getMinNumberIndex(int[] numbers, int index, int i) {
+        int min;
+        min= Integer.MAX_VALUE;
+        for(int j=i; j<numbers.length; j++) {
+           if(numbers[j] < min) {
+               min = numbers[j];
+               index = j;
+           }
+        }
+        return index;
     }
 
     private static void swap(int[] numbers, int index, int i) {
