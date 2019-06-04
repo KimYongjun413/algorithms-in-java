a0a7358d998ebe1250a75f33bd74f2047aef522b
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-04 23:43:57 +0900

    Refactoring selectionSort method

--- src/Sorts/SelectionSort.java
+++ src/Sorts/SelectionSort.java
@@ -4,17 +4,22 @@ public class SelectionSort {
     public static void main(String[] args) {
 
         int[] numbers = {12, 4, 5, 7, 8, 6, 10, 11, 18, 20};
+
+        selectionSort(numbers);
+
+        for(int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+    private static void selectionSort(int[] numbers) {
         int index = 0;
 
         for(int i =0; i< numbers.length; i++) {
             index = getMinNumberIndex(numbers, index, i);
             swap(numbers, index, i);
         }
-
-        for(int num : numbers) {
-            System.out.print(num + " ");
-        }
-
     }
 
     private static int getMinNumberIndex(int[] numbers, int index, int i) {
