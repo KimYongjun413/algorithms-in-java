bc1ae3846bc90481b4fc9e96f13a05d88f17d928
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-15 23:18:05 +0900

    [Create] 하나의 테스트에서 여러 알고리즘을 처리하기 위한 Test 생성

---
+++ test/Sorts/SortTest.java
@@ -0,0 +1,23 @@
+package Sorts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortTest {
+
+    @Test
+    public void SortAlgorithmTest() {
+        Sort[] sorts = {
+                new BubbleSort()
+        };
+
+        Integer[] unsorted = {5, 9, 3, 1, 2, 8, 4, 7, 6};
+        Integer[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (Sort sort : sorts) {
+            assertArrayEquals(sorted, sort.sort(unsorted));
+        }
+    }
+
+}
