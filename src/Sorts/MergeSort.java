53ad7fd8a34129645b41b2e1e90c49c564c4b380
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-15 23:58:19 +0900

    [Refactoring] MergeSort 를 Sort Interface에 맞게 수정

--- src/Sorts/MergeSort.java
+++ src/Sorts/MergeSort.java
@@ -1,32 +1,39 @@
 package Sorts;
 
-public class MergeSort {
+public class MergeSort implements Sort {
     public static void main(String[] args) {
 
-        int numbers[] = {6, 4, 3, 7, 5, 1, 2};
-        int[] temp = new int[numbers.length];
-        showArray(numbers);
-        mergeSort(numbers, temp, 0, numbers.length - 1);
+        Integer[] integers = {6, 4, 3, 7, 5, 1, 2};
 
-        showArray(numbers);
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(integers);
+
+        printArray(integers);
     }
 
-    public static void mergeSort(int[] numbers, int[] temp, int start, int end) {
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        T[] tmp = (T[]) new Comparable[array.length];
+        devide(array, tmp, 0, array.length - 1);
+        return array;
+    }
+
+    public static <T extends Comparable<T>> void devide(T[] numbers, T[] temp, int start, int end) {
         if (start == end) return;
         int mid = start + (end - start) / 2;
-        mergeSort(numbers, temp, start, mid);
-        mergeSort(numbers, temp, mid + 1, end);
+        devide(numbers, temp, start, mid);
+        devide(numbers, temp, mid + 1, end);
         doMerge(numbers, temp, start, mid, end);
     }
 
-    public static void doMerge(int[] numbers, int[] temp, int start, int mid, int end) {
+    public static <T extends Comparable<T>> void doMerge(T[] numbers, T[] temp, int start, int mid, int end) {
         System.arraycopy(numbers, start, temp, start, end - start + 1);
         int leftIdx = start;
         int sortIdx = start;
         int rightIdx = mid + 1;
 
         while (leftIdx <= mid && rightIdx <= end) {
-            if (temp[leftIdx] < temp[rightIdx]) {
+            if (temp[leftIdx].compareTo(temp[rightIdx]) < 0) {
                 numbers[sortIdx++] = temp[leftIdx++];
             } else {
                 numbers[sortIdx++] = temp[rightIdx++];
@@ -43,15 +50,9 @@ public class MergeSort {
 
     }
 
-    public static void swap(int[] numbers, int i, int j) {
-        int temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
-    }
-
-    public static void showArray(int[] numbers) {
-        for (int num : numbers) {
-            System.out.print(num + " ");
+    public static <T> void printArray(T[] array) {
+        for (T arr : array) {
+            System.out.print(arr + " ");
         }
         System.out.println();
     }
