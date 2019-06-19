0efba6686c7a1fa2f0a797fcf4816f1a304c663d
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-20 02:28:31 +0900

    [Create] 문자열에 중복 문자 체크하는 메서드(Quick Sort 전용) 작성

--- src/CodingInterview/ArraysAndStrings/Example_1_1.java
+++ src/CodingInterview/ArraysAndStrings/Example_1_1.java
@@ -12,7 +12,13 @@ public class Example_1_1 {
         String string = "aAbBcCdDeEaA";
 
         boolean result = isDuplicateByAscii(string);
+//        result = isDuplicateBySort(string);
 
+        printResult(result);
+
+    }
+
+    private static void printResult(boolean result) {
         if (result) System.out.println("같은 문자 중복");
         else System.out.println("같은 문자 없음");
     }
@@ -21,11 +27,58 @@ public class Example_1_1 {
 
         boolean[] visited_Ascii = new boolean[128];
         for (int i = 0; i < string.length(); i++) {
-            if(visited_Ascii[string.charAt(i)]) {
+            if (visited_Ascii[string.charAt(i)]) {
                 return true;
             }
             visited_Ascii[string.charAt(i)] = true;
         }
         return false;
+    }
+
+    public static boolean isDuplicateBySort(String string) {
+        char[] chars = string.toCharArray();
+        Character[] characters = new Character[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            characters[i] = chars[i];
+        }
+        sort(characters, 0, chars.length - 1);
+        return isFindDuplicate(characters);
+    }
+
+    public static <T extends Comparable<T>> boolean isFindDuplicate(T[] characters) {
+
+        for (int i = 0; i < characters.length - 2; i++) {
+            if (characters[i].compareTo(characters[i + 1]) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <T extends Comparable<T>> void sort(T[] array, int start, int end) {
+        if (start >= end) return;
+        int r = getPivot(array, start, end);
+        sort(array, start, r - 1);
+        sort(array, r, end);
+    }
+
+    public static <T extends Comparable<T>> int getPivot(T[] numbers, int start, int end) {
+        T pivot = numbers[(start + end) / 2];
+        while (start <= end) {
+            while (numbers[start].compareTo(pivot) < 0) start++;
+            while (numbers[end].compareTo(pivot) > 0) end--;
+            if (start <= end) {
+                swap(numbers, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    public static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
