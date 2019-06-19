8386f17f797aa9eb0486b2525adb0f08f8eb4ac2
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-20 02:28:43 +0900

    [Create] 문자열에 중복 문자 체크하는 메서드(Quick Sort 전용) Test Code 작성

--- test/CodingInterview/ArraysAndStrings/Example_1_1Test.java
+++ test/CodingInterview/ArraysAndStrings/Example_1_1Test.java
@@ -16,4 +16,29 @@ public class Example_1_1Test {
         assertTrue(example.isDuplicateByAscii("A B C"));
         assertFalse(example.isDuplicateByAscii("aAbBcCd DeEfFgG"));
     }
+
+    @Test
+    public void sort() {
+        Character[] chars = {'a', 'c', 'b', 'A'};
+        Character[] expected = {'A', 'a', 'b', 'c'};
+
+        example.sort(chars, 0, chars.length - 1);
+        assertArrayEquals(expected, chars);
+
+        Character[] chars1 = {'a', 'c', 'b', 'A', 'a'};
+        Character[] expected1 = {'A', 'a', 'a', 'b', 'c'};
+        example.sort(chars1, 0, chars1.length - 1);
+        assertArrayEquals(expected1, chars1);
+
+    }
+
+    @Test
+    public void isFindDuplicate() {
+        Character[] expected = {'A', 'a', 'b', 'c'};
+        Character[] expected1 = {'A', 'a', 'a', 'b', 'c'};
+
+        assertFalse(example.isFindDuplicate(expected));
+        assertTrue(example.isFindDuplicate(expected1));
+
+    }
 }
